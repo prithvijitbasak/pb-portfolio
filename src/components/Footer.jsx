bcd7ab788b6edcb83e6221959f252b01983b2ccc
Prithvijit Basak <prithvijitbasak22695@gmail.com>
2025-01-26 22:09:38 +0530

    made footer section, some parts are remaining to be completed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,49 @@
+import { useEffect, useState } from "react";
 import "../assets/styles/Footer.css";
+import logo from "../../images/logo.png";
+import whatsapp from "../../images/whatsapp-logo.svg";
+import linkedin from "../../images/linkedin-logo.svg";
 
 const Footer = () => {
+  const [year, setYear] = useState(null);
+
+  useEffect(() => {
+    const currentYear = new Date().getFullYear();
+    setYear(currentYear);
+  }, []);
+
   return (
     <>
       <section className="pb-footer-wrapper">
         <div className="pb-container">
           <div className="pb-footer-contents">
-          
+            <div className="footer-cols">
+              <div className="footer-logo-div">
+                <img src={logo} alt="Prithvijit Basak Logo" />
+              </div>
+              <div className="socials-and-links">
+                <div className="links-div"></div>
+                <div className="socials-div">
+                  <h3>Connect me on</h3>
+                  <div className="socials-icons">
+                    <img src={linkedin} alt="LinkedIn" />
+                    <img src={whatsapp} alt="WhatsApp" />
+
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="copyright-div">
+              <p>
+                &#169; {year} | Developed by{" "}
+                <a
+                  href="https://www.linkedin.com/in/prithvijitbasak/"
+                  target="_blank"
+                >
+                  Prithvijit Basak
+                </a>{" "}
+              </p>
+            </div>
           </div>
         </div>
       </section>
